b2e046514f839dfaefaa0c9ebffd5dd2d4b05953
dotieuthien <dotieuthien9997@gmail.com>
2023-10-19 22:18:26 +0700

    Update something

---
+++ third_party/opencv/share/OpenCV/OpenCVModules-release.cmake
@@ -0,0 +1,289 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "zlib" for configuration "Release"
+set_property(TARGET zlib APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(zlib PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/share/OpenCV/3rdparty/lib64/libzlib.a"
+  )
+
+list(APPEND _cmake_import_check_targets zlib )
+list(APPEND _cmake_import_check_files_for_zlib "${_IMPORT_PREFIX}/share/OpenCV/3rdparty/lib64/libzlib.a" )
+
+# Import target "libjpeg-turbo" for configuration "Release"
+set_property(TARGET libjpeg-turbo APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libjpeg-turbo PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/share/OpenCV/3rdparty/lib64/liblibjpeg-turbo.a"
+  )
+
+list(APPEND _cmake_import_check_targets libjpeg-turbo )
+list(APPEND _cmake_import_check_files_for_libjpeg-turbo "${_IMPORT_PREFIX}/share/OpenCV/3rdparty/lib64/liblibjpeg-turbo.a" )
+
+# Import target "libtiff" for configuration "Release"
+set_property(TARGET libtiff APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libtiff PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C;CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/share/OpenCV/3rdparty/lib64/liblibtiff.a"
+  )
+
+list(APPEND _cmake_import_check_targets libtiff )
+list(APPEND _cmake_import_check_files_for_libtiff "${_IMPORT_PREFIX}/share/OpenCV/3rdparty/lib64/liblibtiff.a" )
+
+# Import target "libwebp" for configuration "Release"
+set_property(TARGET libwebp APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libwebp PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/share/OpenCV/3rdparty/lib64/liblibwebp.a"
+  )
+
+list(APPEND _cmake_import_check_targets libwebp )
+list(APPEND _cmake_import_check_files_for_libwebp "${_IMPORT_PREFIX}/share/OpenCV/3rdparty/lib64/liblibwebp.a" )
+
+# Import target "libjasper" for configuration "Release"
+set_property(TARGET libjasper APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libjasper PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/share/OpenCV/3rdparty/lib64/liblibjasper.a"
+  )
+
+list(APPEND _cmake_import_check_targets libjasper )
+list(APPEND _cmake_import_check_files_for_libjasper "${_IMPORT_PREFIX}/share/OpenCV/3rdparty/lib64/liblibjasper.a" )
+
+# Import target "libpng" for configuration "Release"
+set_property(TARGET libpng APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libpng PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/share/OpenCV/3rdparty/lib64/liblibpng.a"
+  )
+
+list(APPEND _cmake_import_check_targets libpng )
+list(APPEND _cmake_import_check_files_for_libpng "${_IMPORT_PREFIX}/share/OpenCV/3rdparty/lib64/liblibpng.a" )
+
+# Import target "IlmImf" for configuration "Release"
+set_property(TARGET IlmImf APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(IlmImf PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/share/OpenCV/3rdparty/lib64/libIlmImf.a"
+  )
+
+list(APPEND _cmake_import_check_targets IlmImf )
+list(APPEND _cmake_import_check_files_for_IlmImf "${_IMPORT_PREFIX}/share/OpenCV/3rdparty/lib64/libIlmImf.a" )
+
+# Import target "libprotobuf" for configuration "Release"
+set_property(TARGET libprotobuf APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(libprotobuf PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/share/OpenCV/3rdparty/lib64/liblibprotobuf.a"
+  )
+
+list(APPEND _cmake_import_check_targets libprotobuf )
+list(APPEND _cmake_import_check_files_for_libprotobuf "${_IMPORT_PREFIX}/share/OpenCV/3rdparty/lib64/liblibprotobuf.a" )
+
+# Import target "quirc" for configuration "Release"
+set_property(TARGET quirc APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(quirc PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/share/OpenCV/3rdparty/lib64/libquirc.a"
+  )
+
+list(APPEND _cmake_import_check_targets quirc )
+list(APPEND _cmake_import_check_files_for_quirc "${_IMPORT_PREFIX}/share/OpenCV/3rdparty/lib64/libquirc.a" )
+
+# Import target "tegra_hal" for configuration "Release"
+set_property(TARGET tegra_hal APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(tegra_hal PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/share/OpenCV/3rdparty/lib64/libtegra_hal.a"
+  )
+
+list(APPEND _cmake_import_check_targets tegra_hal )
+list(APPEND _cmake_import_check_files_for_tegra_hal "${_IMPORT_PREFIX}/share/OpenCV/3rdparty/lib64/libtegra_hal.a" )
+
+# Import target "ittnotify" for configuration "Release"
+set_property(TARGET ittnotify APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ittnotify PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/share/OpenCV/3rdparty/lib64/libittnotify.a"
+  )
+
+list(APPEND _cmake_import_check_targets ittnotify )
+list(APPEND _cmake_import_check_files_for_ittnotify "${_IMPORT_PREFIX}/share/OpenCV/3rdparty/lib64/libittnotify.a" )
+
+# Import target "opencv_core" for configuration "Release"
+set_property(TARGET opencv_core APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_core PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libopencv_core.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_core )
+list(APPEND _cmake_import_check_files_for_opencv_core "${_IMPORT_PREFIX}/lib64/libopencv_core.a" )
+
+# Import target "opencv_flann" for configuration "Release"
+set_property(TARGET opencv_flann APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_flann PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libopencv_flann.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_flann )
+list(APPEND _cmake_import_check_files_for_opencv_flann "${_IMPORT_PREFIX}/lib64/libopencv_flann.a" )
+
+# Import target "opencv_imgproc" for configuration "Release"
+set_property(TARGET opencv_imgproc APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_imgproc PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libopencv_imgproc.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_imgproc )
+list(APPEND _cmake_import_check_files_for_opencv_imgproc "${_IMPORT_PREFIX}/lib64/libopencv_imgproc.a" )
+
+# Import target "opencv_ml" for configuration "Release"
+set_property(TARGET opencv_ml APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_ml PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libopencv_ml.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_ml )
+list(APPEND _cmake_import_check_files_for_opencv_ml "${_IMPORT_PREFIX}/lib64/libopencv_ml.a" )
+
+# Import target "opencv_photo" for configuration "Release"
+set_property(TARGET opencv_photo APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_photo PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libopencv_photo.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_photo )
+list(APPEND _cmake_import_check_files_for_opencv_photo "${_IMPORT_PREFIX}/lib64/libopencv_photo.a" )
+
+# Import target "opencv_video" for configuration "Release"
+set_property(TARGET opencv_video APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_video PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libopencv_video.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_video )
+list(APPEND _cmake_import_check_files_for_opencv_video "${_IMPORT_PREFIX}/lib64/libopencv_video.a" )
+
+# Import target "opencv_dnn" for configuration "Release"
+set_property(TARGET opencv_dnn APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_dnn PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libopencv_dnn.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_dnn )
+list(APPEND _cmake_import_check_files_for_opencv_dnn "${_IMPORT_PREFIX}/lib64/libopencv_dnn.a" )
+
+# Import target "opencv_features2d" for configuration "Release"
+set_property(TARGET opencv_features2d APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_features2d PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libopencv_features2d.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_features2d )
+list(APPEND _cmake_import_check_files_for_opencv_features2d "${_IMPORT_PREFIX}/lib64/libopencv_features2d.a" )
+
+# Import target "opencv_imgcodecs" for configuration "Release"
+set_property(TARGET opencv_imgcodecs APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_imgcodecs PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libopencv_imgcodecs.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_imgcodecs )
+list(APPEND _cmake_import_check_files_for_opencv_imgcodecs "${_IMPORT_PREFIX}/lib64/libopencv_imgcodecs.a" )
+
+# Import target "opencv_shape" for configuration "Release"
+set_property(TARGET opencv_shape APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_shape PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libopencv_shape.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_shape )
+list(APPEND _cmake_import_check_files_for_opencv_shape "${_IMPORT_PREFIX}/lib64/libopencv_shape.a" )
+
+# Import target "opencv_videoio" for configuration "Release"
+set_property(TARGET opencv_videoio APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_videoio PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libopencv_videoio.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_videoio )
+list(APPEND _cmake_import_check_files_for_opencv_videoio "${_IMPORT_PREFIX}/lib64/libopencv_videoio.a" )
+
+# Import target "opencv_calib3d" for configuration "Release"
+set_property(TARGET opencv_calib3d APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_calib3d PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libopencv_calib3d.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_calib3d )
+list(APPEND _cmake_import_check_files_for_opencv_calib3d "${_IMPORT_PREFIX}/lib64/libopencv_calib3d.a" )
+
+# Import target "opencv_highgui" for configuration "Release"
+set_property(TARGET opencv_highgui APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_highgui PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libopencv_highgui.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_highgui )
+list(APPEND _cmake_import_check_files_for_opencv_highgui "${_IMPORT_PREFIX}/lib64/libopencv_highgui.a" )
+
+# Import target "opencv_objdetect" for configuration "Release"
+set_property(TARGET opencv_objdetect APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_objdetect PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libopencv_objdetect.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_objdetect )
+list(APPEND _cmake_import_check_files_for_opencv_objdetect "${_IMPORT_PREFIX}/lib64/libopencv_objdetect.a" )
+
+# Import target "opencv_stitching" for configuration "Release"
+set_property(TARGET opencv_stitching APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_stitching PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libopencv_stitching.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_stitching )
+list(APPEND _cmake_import_check_files_for_opencv_stitching "${_IMPORT_PREFIX}/lib64/libopencv_stitching.a" )
+
+# Import target "opencv_superres" for configuration "Release"
+set_property(TARGET opencv_superres APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_superres PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libopencv_superres.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_superres )
+list(APPEND _cmake_import_check_files_for_opencv_superres "${_IMPORT_PREFIX}/lib64/libopencv_superres.a" )
+
+# Import target "opencv_videostab" for configuration "Release"
+set_property(TARGET opencv_videostab APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(opencv_videostab PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libopencv_videostab.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_videostab )
+list(APPEND _cmake_import_check_files_for_opencv_videostab "${_IMPORT_PREFIX}/lib64/libopencv_videostab.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
